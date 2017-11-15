63459c5f25b534b1409b0191b57ec28a88032c9d
dimitrovaElka <dimitrova_elka@abv.bg>
2017-11-15 15:52:53 +0200

    Main method

--- StartUp/StartUp.cs
+++ StartUp/StartUp.cs
@@ -6,7 +6,30 @@ namespace ListProcessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputLine = Console.ReadLine().Split(' ').ToList();
+            Console.WriteLine(String.Join(' ', inputLine));
+			var commandInput = "";
+			while ((commandInput = Console.ReadLine()) != "end")
+			{
+				var commands = commandInput.Split(' ');
+				switch(commands[0])
+				{
+					case "append": AddStringAtTheEnd(commands[1], inputLine); break;
+					case "prepend": InsertStringInTheStart(commands[1], inputLine); break;
+					case "reverse": ReverseList(inputLine); break;
+					case "insert": InsertStringAtThePosition(inputLine, commands[1]); break;
+					case "delete": DeleteIndex(inputLine, commands[1]); break;
+					case "roll": 
+						{
+							if (commands[1] == "left") RollLeft(inputLine);
+							else RollRight(inputLine);
+						}
+						break;
+					case "sort": SortList(inputLine);break;
+					case "count": CountString(inputLine, commands[1]);break;
+					default: ErrorMessage(); break;
+				}
+			}
         }
     }
 }
